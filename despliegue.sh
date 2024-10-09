8c9335797ce1593bb285e9d7588ce0fabe9eff16
MAlejandroR <manuelromeromiguel@gmail.com>
2024-10-09 18:51:05 +0200

    Actualizando

--- despliegue.sh
+++ despliegue.sh
@@ -6,10 +6,10 @@ git commit -m "$1"
 git push origin main
 
 # Elimina el directorio public
-sudo rm -r ./public
+sudo rm -r ./docs
 
 # Genera el sitio con Hugo
-hugo -d public
+hugo -d docs
 
 # Sincroniza los archivos al servidor remoto usando rsync con puerto SSH 22123
-rsync -avz -e "ssh -p 22123" ./public/ admin@web.infenlaces.com:web/web.infenlaces.com/public_html/despliegue/
+rsync -avz -e "ssh -p 22123" ./docs/ admin@web.infenlaces.com:web/web.infenlaces.com/public_html/despliegue/
